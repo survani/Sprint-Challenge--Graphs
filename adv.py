9607a1cce1b6431bd03619364662739706db5a85
survani <giovanicode@outlook.com>
2020-11-20 20:50:42 -0800

    MVP completed 990+ but less than 2000...

--- adv.py
+++ adv.py
@@ -28,6 +28,56 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# backwords path...
+returnPath = []
+revDirections = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+# create a set for the rooms that were already visited...
+visited = set()
+
+# if the length of the room_graph -1 is less than the length of the visited -1 continue..
+while len(room_graph) - 1 > len(visited) - 1:
+    # set what upcoming move as a variable
+    # default it to 0 or None to init the start of the variable...
+    # This will be helpful when writting my if else statement...
+    upcoming_move = 0
+
+    # for loop player to continue after exiting the current room...
+    for continuing in player.current_room.get_exits():
+        # checking...
+        if player.current_room.get_room_in_direction(continuing) not in visited:
+            # re-update the variable upcoming_move to continuing...
+            upcoming_move = continuing
+
+    # checks if the variable upcoming_move has initiated...
+    if upcoming_move is not 0:
+        # add the upcoming move to the traversal path at the end...
+        traversal_path.append(upcoming_move)
+
+        # more appending done...
+        returnPath.append(revDirections[upcoming_move])
+
+        # make the player travel with the upcoming moves...
+        player.travel(upcoming_move)
+
+        # adds the visited places the player has gone through...
+        visited.add(player.current_room)
+
+    # else if no more moves left...
+    else:
+
+        # pop of the returnPath and set it to the upcoming move...
+        upcoming_move = returnPath.pop()
+
+        # append what was pop from the return path to the traversal_path...
+        traversal_path.append(upcoming_move)
+
+        # make the player travel with the upcoming moves...
+        player.travel(upcoming_move)
+
+# Uncomment to see where the player visited while the while loop occurs...
+#     print(player.current_room)
+
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
